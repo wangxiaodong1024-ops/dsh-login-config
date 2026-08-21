2d330f551817f6721a60c2852494bb8238a63dfd
wangxiaodong1024-ops <wangxiaodong1024@gmail.com>
2026-08-21 21:40:49 +0800

    Update baibo.js

--- baibo.js
+++ baibo.js
@@ -34,6 +34,14 @@ window.__dsh3 = {
           "pwdCipher": "wq7DpsOhwqXCmMKnwqzCosKwwrzCrMKeXWRm",
           "hasCaptcha": true,
           "hideSave": true
+        },
+         {
+          "name": "上药控股镇江有限公司",
+          "host": "www.jsjtyy.com",
+          "account": "hzzmhd",
+          "pwdCipher": "wpTCo8KYXcKRwqU=",
+          "hasCaptcha": false,
+          "hideSave": true
         }
       ]
     }
