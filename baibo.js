b84fb286a95df06012f3f379057b5d61fc637c88
wangxiaodong1024-ops <wangxiaodong1024@gmail.com>
2026-08-21 21:05:56 +0800

    Add files via upload

---
+++ baibo.js
@@ -0,0 +1,41 @@
+window.__dsh3 = {
+  "people": {
+    "白波": {
+      "enabled": true,
+      "sites": [
+        {
+          "name": "国药控股无锡有限公司",
+          "host": "61.177.117.158:9098",
+          "account": "100388",
+          "pwdCipher": "wq7DhsOBwoXCkcKswqbCp8KZwp/CrA==",
+          "hasCaptcha": true,
+          "hideSave": true
+        },
+        {
+          "name": "华润江苏医药有限公司",
+          "host": "lxhrjs.crpcg.com",
+          "account": "HZZMHD",
+          "pwdCipher": "wq7DpsOhwqXCmMKnwqzCosKwwrzCrMKeXWRm",
+          "hasCaptcha": true,
+          "hideSave": true
+        },
+        {
+          "name": "华润昆山医药有限公司",
+          "host": "lxhrjs.crpcg.com",
+          "account": "51100826",
+          "pwdCipher": "wq7DpsOhwqXCmMKnwqzCosKwwrzCrMKeXWRm",
+          "hasCaptcha": true,
+          "hideSave": true
+        },
+        {
+          "name": "华润张家港百禾医药有限公司",
+          "host": "lxhrjs.crpcg.com",
+          "account": "中美华东1",
+          "pwdCipher": "wq7DpsOhwqXCmMKnwqzCosKwwrzCrMKeXWRm",
+          "hasCaptcha": true,
+          "hideSave": true
+        }
+      ]
+    }
+  }
+};
