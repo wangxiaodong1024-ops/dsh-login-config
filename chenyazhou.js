19227b25add8f7e2803ada2658a14fe77a0064ad
wangxiaodong1024-ops <wangxiaodong1024@gmail.com>
2026-08-21 21:23:10 +0800

    Add files via upload

---
+++ chenyazhou.js
@@ -0,0 +1,81 @@
+window.__dsh3 = {
+  "people": {
+    "陈亚洲": {
+      "enabled": true,
+      "sites": [
+        {
+          "name": "江苏省医药有限公司",
+          "host": "lx.js-pc.com.cn",
+          "account": "hzzmhd",
+          "pwdCipher": "wq7DpsOhwqXCmMKnwqzCosKwwrw=",
+          "hasCaptcha": true,
+          "hideSave": true
+        },
+        {
+          "name": "南京药业股份有限公司",
+          "host": "222.190.117.9",
+          "account": "1003528",
+          "pwdCipher": "wq7DpsOhwqXCmMKnwqzCosKwwrzCrMKeXWRm",
+          "hasCaptcha": false,
+          "hideSave": true
+        },
+        {
+          "name": "南京医药股份有限公司镇江分公司",
+          "host": "222.190.117.9",
+          "account": "hzzmhdzy",
+          "pwdCipher": "wq7DpsOhwqXCmMKnwqzCosKwwrzCrMKeXWRm",
+          "hasCaptcha": false,
+          "hideSave": true
+        },
+        {
+          "name": "南京医药股份有限公司",
+          "host": "222.190.117.9",
+          "account": "1022002",
+          "pwdCipher": "wq7DpsOhwqXCmMKnwqzCosKwwrzCrMKeXWRm",
+          "hasCaptcha": false,
+          "hideSave": true
+        },
+        {
+          "name": "国药控股镇江有限公司",
+          "host": "flow.sinopharm-yz.com:8083",
+          "account": "杭州中美华东ZJ123",
+          "pwdCipher": "wq7DpsOhwqXCmcKowqrCucK5wqnCnsKcX2g=",
+          "hasCaptcha": false,
+          "hideSave": true
+        },
+        {
+          "name": "国药控股扬州有限公司",
+          "host": "flow.sinopharm-yz.com:8081",
+          "account": "中美华东",
+          "pwdCipher": "wq7DpsOhwqXCmcKowqrCucK5wqnCnsKcX2g=",
+          "hasCaptcha": false,
+          "hideSave": true
+        },
+        {
+          "name": "华润扬州医药有限公司",
+          "host": "lxhrjs.crpcg.com",
+          "account": "华东",
+          "pwdCipher": "wq7DpsOhwqXCmMKnwqzCosKwwrzCrMKeXWRm",
+          "hasCaptcha": true,
+          "hideSave": true
+        },
+        {
+          "name": "江苏九州通医药有限公司",
+          "host": "flow.jztit.com",
+          "account": "hzzmhdcf",
+          "pwdCipher": "wq7DpsOhwqXCmcKrwr7DgsKmwpvCog==",
+          "hasCaptcha": false,
+          "hideSave": true
+        },
+        {
+          "name": "华润江苏医药有限公司",
+          "host": "lxhrjs.crpcg.com",
+          "account": "HZZMHD",
+          "pwdCipher": "wq7DpsOhwqXCmMKnwqzCosKwwrzCrMKeXWRm",
+          "hasCaptcha": true,
+          "hideSave": true
+        }
+      ]
+    }
+  }
+};
