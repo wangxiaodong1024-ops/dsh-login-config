5efc9ae886ac8b8a3d118a3ad97abe22fd38080a
wangxiaodong1024-ops <wangxiaodong1024@gmail.com>
2026-08-22 16:19:42 +0800

    Add files via upload

---
+++ wulei.js
@@ -0,0 +1,54 @@
+window.__dsh3 = {
+  "people": {
+    "吴雷": {
+      "enabled": true,
+      "sites": [
+        {
+          "name": "江苏省医药有限公司",
+          "url": "https://lx.js-pc.com.cn/#/login",
+          "host": "lx.js-pc.com.cn",
+          "account": "hzzmhd",
+          "pwdCipher": "wq7DpsOhwqXCmMKnwqzCosKwwrw=",
+          "hasCaptcha": true,
+          "hideSave": true
+        },
+        {
+          "name": "南京医药(淮安)天颐有限公司",
+          "url": "http://222.190.117.9/member/haty",
+          "host": "222.190.117.9",
+          "account": "zhongmhd",
+          "pwdCipher": "wq7DpsOhwqXCmMKnwqzCosKwwrzCrMKeXWRm",
+          "hasCaptcha": false,
+          "hideSave": true
+        },
+        {
+          "name": "江苏华晓医药有限公司",
+          "url": "http://222.190.117.9/member/hxyy",
+          "host": "222.190.117.9",
+          "account": "10251",
+          "pwdCipher": "wq7DpsOhwqXCmMKnwqzCosKwwrzCrMKeXWRm",
+          "hasCaptcha": false,
+          "hideSave": true
+        },
+        {
+          "name": "华润南通医药有限公司",
+          "url": "http://lxhrjs.crpcg.com",
+          "host": "lxhrjs.crpcg.com",
+          "account": "hzzm",
+          "pwdCipher": "wq7DpsOhwqXCmMKnwqzCosKwwrzCrMKeXWRm",
+          "hasCaptcha": true,
+          "hideSave": true
+        },
+        {
+          "name": "国药控股盐城有限公司",
+          "url": "https://shop.gkyc.cn/strack/login.jsp",
+          "host": "shop.gkyc.cn",
+          "account": "gkyc137",
+          "pwdCipher": "wq7DpsOhwqXCmcKowqrCucK5wqnCnsKcX2g=",
+          "hasCaptcha": true,
+          "hideSave": true
+        }
+      ]
+    }
+  }
+};
