520bb4b2c5f4bccd50e8b94946ff8afa5325031e
wangxiaodong1024-ops <wangxiaodong1024@gmail.com>
2026-08-23 22:47:43 +0800

    Add files via upload

---
+++ wangfang.js
@@ -0,0 +1,108 @@
+window.__dsh3 = {
+  "people": {
+    "王芳": {
+      "enabled": true,
+      "sites": [
+        {
+          "name": "国药控股无锡有限公司",
+          "url": "http://61.177.117.158:9098/WebGXLS.aspx",
+          "host": "61.177.117.158:9098",
+          "account": "100388",
+          "pwdCipher": "wq7DhsOBwoXCkcKswqbCp8KZwp/CrA==",
+          "hasCaptcha": true,
+          "hideSave": true
+        },
+        {
+          "name": "国药控股常州有限公司",
+          "url": "http://218.93.53.116:8086/sinopharmcz/client/userlogin.jsp",
+          "host": "218.93.53.116:8086",
+          "account": "ZMHDGS",
+          "pwdCipher": "wq7DpsOhwqXCmcKowqrCucK5wqnCnsKcX2g=",
+          "hasCaptcha": true,
+          "hideSave": true
+        },
+        {
+          "name": "华润江苏医药有限公司",
+          "url": "http://lxhrjs.crpcg.com",
+          "host": "lxhrjs.crpcg.com",
+          "account": "HZZMHD",
+          "pwdCipher": "wq7DpsOhwqXCmMKnwqzCosKwwrzCrMKeXWRm",
+          "hasCaptcha": true,
+          "hideSave": true
+        },
+        {
+          "name": "华润昆山医药有限公司",
+          "url": "http://lxhrjs.crpcg.com",
+          "host": "lxhrjs.crpcg.com",
+          "account": "51100826",
+          "pwdCipher": "wq7DpsOhwqXCmMKnwqzCosKwwrzCrMKeXWRm",
+          "hasCaptcha": true,
+          "hideSave": true
+        },
+        {
+          "name": "华润张家港百禾医药有限公司",
+          "url": "http://lxhrjs.crpcg.com",
+          "host": "lxhrjs.crpcg.com",
+          "account": "中美华东1",
+          "pwdCipher": "wq7DpsOhwqXCmMKnwqzCosKwwrzCrMKeXWRm",
+          "hasCaptcha": true,
+          "hideSave": true
+        },
+        {
+          "name": "华润南通医药有限公司",
+          "url": "http://lxhrjs.crpcg.com",
+          "host": "lxhrjs.crpcg.com",
+          "account": "hzzm",
+          "pwdCipher": "wq7DpsOhwqXCmMKnwqzCosKwwrzCrMKeXWRm",
+          "hasCaptcha": true,
+          "hideSave": true
+        },
+        {
+          "name": "华润无锡医药有限公司",
+          "url": "http://lxhrjs.crpcg.com",
+          "host": "lxhrjs.crpcg.com",
+          "account": "hzzmhd",
+          "pwdCipher": "wq7DpsOhwqXCmMKnwqzCosKwwrzCrMKeXWRm",
+          "hasCaptcha": true,
+          "hideSave": true
+        },
+        {
+          "name": "华润无锡医药有限公司江阴分公司",
+          "url": "http://lxhrjs.crpcg.com",
+          "host": "lxhrjs.crpcg.com",
+          "account": "zmhdjy",
+          "pwdCipher": "wqzDpcOSwqbCksKmwqTCo8Km",
+          "hasCaptcha": true,
+          "hideSave": true
+        },
+        {
+          "name": "南京医药南通健桥有限公司",
+          "url": "http://222.190.117.9/member/ntjq",
+          "host": "222.190.117.9",
+          "account": "njjygf",
+          "pwdCipher": "wq7DpsOhwqXCmMKnwqzCosKwwrzCrMKeXWRm",
+          "hasCaptcha": false,
+          "hideSave": true
+        },
+        {
+          "name": "上药集团常州药业股份有限公司",
+          "url": "http://58.216.198.50:8012/",
+          "host": "58.216.198.50:8012",
+          "account": "凌岚",
+          "pwdCipher": "wpvCpcKgYMKRwqs=",
+          "hasCaptcha": false,
+          "hideSave": true
+        },
+        {
+          "name": "江苏澳洋医药物流公司",
+          "url": "http://fr.aoyang.com/WebReport/decision",
+          "host": "fr.aoyang.com",
+          "account": "HZZMHD",
+          "pwdCipher": "wpzCq8Kg",
+          "hasCaptcha": false,
+          "hideSave": true
+        }
+      ]
+    }
+  }
+};
