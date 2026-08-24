607f0950f93c6680659274cc3c06497a6144e232
wangxiaodong1024-ops <wangxiaodong1024@gmail.com>
2026-08-25 00:18:52 +0800

    Add files via upload

---
+++ lisi.js
@@ -0,0 +1,37 @@
+window.__dsh3 = {
+  "people": {
+    "李四": {
+      "enabled": true,
+      "check": "w4TCj8ODfcK1w5E=",
+      "sites": [
+        {
+          "name": "国药控股无锡有限公司",
+          "url": "http://61.177.117.158:9098/WebGXLS.aspx",
+          "host": "61.177.117.158:9098",
+          "account": "100388",
+          "pwdCipher": "wq/Cg8ONworChsKpaMKuwoHCrsKa",
+          "hasCaptcha": true,
+          "hideSave": true
+        },
+        {
+          "name": "国药控股常州有限公司",
+          "url": "http://218.93.53.116:8086/sinopharmcz/client/userlogin.jsp",
+          "host": "218.93.53.116:8086",
+          "account": "ZMHDGS",
+          "pwdCipher": "wq/Co8OtwqrCjsKlbMOAwqHCuMKMaMKZbw==",
+          "hasCaptcha": true,
+          "hideSave": true
+        },
+        {
+          "name": "国药控股泰州有限公司",
+          "url": "http://flow.sinopharm-yz.com:8082/login.aspx",
+          "host": "flow.sinopharm-yz.com:8082",
+          "account": "杭州中美华东",
+          "pwdCipher": "wqV6w4fCi8KuwqppwqzCjsOCwq1qwpdrfw==",
+          "hasCaptcha": false,
+          "hideSave": true
+        }
+      ]
+    }
+  }
+};
