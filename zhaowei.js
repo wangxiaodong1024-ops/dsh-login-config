14e78c04a8534e9a7ab6a0fa7ab277807be645c5
wangxiaodong1024-ops <wangxiaodong1024@gmail.com>
2026-08-24 18:25:20 +0800

    Update zhaowei.js

--- zhaowei.js
+++ zhaowei.js
@@ -5,6 +5,7 @@ window.__dsh3 = {
       "sites": [
         {
           "name": "南京药业股份有限公司",
+          "url": "http://222.190.117.9/member/yaoye",
           "host": "222.190.117.9",
           "account": "1003528",
           "pwdCipher": "wq7DpsOhwqXCmMKnwqzCosKwwrzCrMKeXWRm",
@@ -13,6 +14,7 @@ window.__dsh3 = {
         },
         {
           "name": "江苏省医药有限公司",
+          "url": "https://lx.js-pc.com.cn/#/login",
           "host": "lx.js-pc.com.cn",
           "account": "hzzmhd",
           "pwdCipher": "wq7DpsOhwqXCmMKnwqzCosKwwrw=",
@@ -21,6 +23,7 @@ window.__dsh3 = {
         },
         {
           "name": "国药控股徐州有限公司",
+          "url": "https://www1.igyxz.com/",
           "host": "www1.igyxz.com",
           "account": "hzzmhd",
           "pwdCipher": "wq7DpsOhwqXCmcKowqrCucK5wqnCnsKcX2g=",
@@ -29,6 +32,7 @@ window.__dsh3 = {
         },
         {
           "name": "国药控股盐城有限公司",
+          "url": "https://shop.gkyc.cn/strack/login.jsp",
           "host": "shop.gkyc.cn",
           "account": "gkyc137",
           "pwdCipher": "wq7DpsOhwqXCmcKowqrCucK5wqnCnsKcX2g=",
@@ -37,6 +41,7 @@ window.__dsh3 = {
         },
         {
           "name": "华润南京医药有限公司",
+          "url": "http://lxhrjs.crpcg.com",
           "host": "lxhrjs.crpcg.com",
           "account": "hr-hd",
           "pwdCipher": "wq7DpsOhwqXCmMKnwqzCosKwwrzCrMKeXWRm",
@@ -45,10 +50,29 @@ window.__dsh3 = {
         },
         {
           "name": "华润徐州医药有限公司",
+          "url": "http://lxhrjs.crpcg.com",
           "host": "lxhrjs.crpcg.com",
           "account": "zmhd102",
           "pwdCipher": "wqzDpcOSwqDCocKnwqrCp8KewpvCnMKeYw==",
           "hasCaptcha": true,
+          "hideSave": true
+        },
+        {
+          "name": "南京医药(淮安)天颐有限公司",
+          "url": "http://222.190.117.9/member/haty",
+          "host": "222.190.117.9",
+          "account": "zhongmhd",
+          "pwdCipher": "wq7DpsOhwqXCmMKnwqzCosKwwrzCrMKeXWRm",
+          "hasCaptcha": false,
+          "hideSave": true
+        },
+        {
+          "name": "宿迁瑞康医药有限公司",
+          "url": "http://222.187.213.150:8086/index/login/index.html",
+          "host": "222.187.213.150:8086",
+          "account": "中美华东",
+          "pwdCipher": "wq7DpsOhwqXCmMKnwqzCosKcwrPCv8Ks",
+          "hasCaptcha": false,
           "hideSave": true
         }
       ]
