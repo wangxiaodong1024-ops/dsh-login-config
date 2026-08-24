d304f435923150ec7c25c837da3c8616a38d2fd0
wangxiaodong1024-ops <wangxiaodong1024@gmail.com>
2026-08-24 15:05:34 +0800

    Add files via upload

---
+++ yanyonghe.js
@@ -0,0 +1,54 @@
+window.__dsh3 = {
+  "people": {
+    "闫泳合": {
+      "enabled": true,
+      "sites": [
+        {
+          "name": "华润徐州医药有限公司",
+          "url": "http://lxhrjs.crpcg.com",
+          "host": "lxhrjs.crpcg.com",
+          "account": "zmhd102",
+          "pwdCipher": "wqzDpcOSwqDCocKnwqrCp8KewpvCnMKeYw==",
+          "hasCaptcha": true,
+          "hideSave": true
+        },
+        {
+          "name": "江苏华美医药有限责任公司",
+          "url": "http://www.jshmlx.com/vendor/login",
+          "host": "www.jshmlx.com",
+          "account": "zhangwei",
+          "pwdCipher": "wq7DpsOhwqXCmMKnwqzCosKwwrzCnsKcX2g=",
+          "hasCaptcha": false,
+          "hideSave": true
+        },
+        {
+          "name": "国药控股徐州有限公司",
+          "url": "https://www1.igyxz.com/",
+          "host": "www1.igyxz.com",
+          "account": "hzzmhd",
+          "pwdCipher": "wq7DpsOhwqXCmcKowqrCucK5wqnCnsKcX2g=",
+          "hasCaptcha": true,
+          "hideSave": true
+        },
+        {
+          "name": "江苏海王集团医药有限公司",
+          "url": "http://222.175.54.18:9999/hwzzwms/mainFrame.html",
+          "host": "222.175.54.18:9999",
+          "account": "lxjshwzmhd",
+          "pwdCipher": "wq7DpsOhwqXCmMKnwqzCosKwwrzCnsKcX2g=",
+          "hasCaptcha": false,
+          "hideSave": true
+        },
+        {
+          "name": "江苏九州通医药有限公司",
+          "url": "http://flow.jztit.com/user/login.html",
+          "host": "flow.jztit.com",
+          "account": "hzzmhdcf",
+          "pwdCipher": "wq7DpsOhwqXCmcKrwr7DgsKmwpvCog==",
+          "hasCaptcha": false,
+          "hideSave": true
+        }
+      ]
+    }
+  }
+};
