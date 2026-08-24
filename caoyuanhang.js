2cc442cd74e519794ee6cf90206c5f36608f0f85
wangxiaodong1024-ops <wangxiaodong1024@gmail.com>
2026-08-24 12:51:15 +0800

    Add files via upload

---
+++ caoyuanhang.js
@@ -0,0 +1,18 @@
+window.__dsh3 = {
+  "people": {
+    "曹远航": {
+      "enabled": true,
+      "sites": [
+        {
+          "name": "国药控股无锡有限公司",
+          "url": "http://61.177.117.158:9098/WebGXLS.aspx",
+          "host": "61.177.117.158:9098",
+          "account": "100388",
+          "pwdCipher": "wq7DhsOBwoXCkcKswqbCp8KZwp/CrA==",
+          "hasCaptcha": true,
+          "hideSave": true
+        }
+      ]
+    }
+  }
+};
---
+++ zhongxiang.js
@@ -0,0 +1,36 @@
+window.__dsh3 = {
+  "people": {
+    "仲祥": {
+      "enabled": true,
+      "sites": [
+        {
+          "name": "国药控股泰州有限公司",
+          "url": "http://flow.sinopharm-yz.com:8082/login.aspx",
+          "host": "flow.sinopharm-yz.com:8082",
+          "account": "杭州中美华东",
+          "pwdCipher": "wqTCvcK7wobCucKtwqfCpcKmwrPCv8KeXWRm",
+          "hasCaptcha": false,
+          "hideSave": true
+        },
+        {
+          "name": "国药控股扬州有限公司",
+          "url": "http://flow.sinopharm-yz.com:8081/login.aspx",
+          "host": "flow.sinopharm-yz.com:8081",
+          "account": "中美华东",
+          "pwdCipher": "wq7DpsOhwqXCmcKowqrCucK5wqnCnsKcX2g=",
+          "hasCaptcha": false,
+          "hideSave": true
+        },
+        {
+          "name": "南京医药扬州有限公司",
+          "url": "http://222.190.117.9/member/yzyy/",
+          "host": "222.190.117.9",
+          "account": "hzzmhd",
+          "pwdCipher": "wq7DpsOhwqXCmMKnwqzCosKwwrzCrMKeXWRm",
+          "hasCaptcha": false,
+          "hideSave": true
+        }
+      ]
+    }
+  }
+};
