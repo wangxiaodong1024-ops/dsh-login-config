35b1631aef796a4b284c50392f1fe5a733de1315
wangxiaodong1024-ops <wangxiaodong1024@gmail.com>
2026-08-24 20:56:21 +0800

    Add files via upload

---
+++ zhangsan.js
@@ -0,0 +1,19 @@
+window.__dsh3 = {
+  "people": {
+    "张三": {
+      "enabled": true,
+      "check": "w4/CkcKjwr7CpsOK",
+      "sites": [
+        {
+          "name": "国药控股无锡有限公司",
+          "url": "http://61.177.117.158:9098/WebGXLS.aspx",
+          "host": "61.177.117.158:9098",
+          "account": "100388",
+          "pwdCipher": "wrrChcKtw4t3wqLCnMKiaHlw",
+          "hasCaptcha": true,
+          "hideSave": true
+        }
+      ]
+    }
+  }
+};
