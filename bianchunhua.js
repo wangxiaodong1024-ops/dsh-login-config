46f464c07af08721f1cf27d14e34aa62e74aebfe
wangxiaodong1024-ops <wangxiaodong1024@gmail.com>
2026-08-24 13:44:34 +0800

    Add files via upload

---
+++ bianchunhua.js
@@ -0,0 +1,45 @@
+window.__dsh3 = {
+  "people": {
+    "卞春华": {
+      "enabled": true,
+      "sites": [
+        {
+          "name": "江苏华晓医药有限公司",
+          "url": "http://222.190.117.9/member/hxyy",
+          "host": "222.190.117.9",
+          "account": "10251",
+          "pwdCipher": "wq7DpsOhwqXCmMKnwqzCosKwwrzCrMKeXWRm",
+          "hasCaptcha": false,
+          "hideSave": true
+        },
+        {
+          "name": "南京医药(淮安)天颐有限公司",
+          "url": "http://222.190.117.9/member/haty",
+          "host": "222.190.117.9",
+          "account": "zhongmhd",
+          "pwdCipher": "wq7DpsOhwqXCmMKnwqzCosKwwrzCrMKeXWRm",
+          "hasCaptcha": false,
+          "hideSave": true
+        },
+        {
+          "name": "国药控股徐州有限公司",
+          "url": "https://www1.igyxz.com/",
+          "host": "www1.igyxz.com",
+          "account": "hzzmhd",
+          "pwdCipher": "wq7DpsOhwqXCmcKowqrCucK5wqnCnsKcX2g=",
+          "hasCaptcha": true,
+          "hideSave": true
+        },
+        {
+          "name": "国药控股盐城有限公司",
+          "url": "https://shop.gkyc.cn/strack/login.jsp",
+          "host": "shop.gkyc.cn",
+          "account": "gkyc137",
+          "pwdCipher": "wq7DpsOhwqXCmcKowqrCucK5wqnCnsKcX2g=",
+          "hasCaptcha": true,
+          "hideSave": true
+        }
+      ]
+    }
+  }
+};
---
+++ wusan.js
@@ -0,0 +1,63 @@
+window.__dsh3 = {
+  "people": {
+    "吴姗": {
+      "enabled": true,
+      "sites": [
+        {
+          "name": "南京药业股份有限公司",
+          "url": "http://222.190.117.9/member/yaoye",
+          "host": "222.190.117.9",
+          "account": "1003528",
+          "pwdCipher": "wq7DpsOhwqXCmMKnwqzCosKwwrzCrMKeXWRm",
+          "hasCaptcha": false,
+          "hideSave": true
+        },
+        {
+          "name": "江苏省医药有限公司",
+          "url": "https://lx.js-pc.com.cn/#/login",
+          "host": "lx.js-pc.com.cn",
+          "account": "hzzmhd",
+          "pwdCipher": "wq7DpsOhwqXCmMKnwqzCosKwwrw=",
+          "hasCaptcha": true,
+          "hideSave": true
+        },
+        {
+          "name": "国药控股泰州有限公司",
+          "url": "http://flow.sinopharm-yz.com:8082/login.aspx",
+          "host": "flow.sinopharm-yz.com:8082",
+          "account": "杭州中美华东",
+          "pwdCipher": "wqTCvcK7wobCucKtwqfCpcKmwrPCv8KeXWRm",
+          "hasCaptcha": false,
+          "hideSave": true
+        },
+        {
+          "name": "国药控股扬州有限公司",
+          "url": "http://flow.sinopharm-yz.com:8081/login.aspx",
+          "host": "flow.sinopharm-yz.com:8081",
+          "account": "中美华东",
+          "pwdCipher": "wq7DpsOhwqXCmcKowqrCucK5wqnCnsKcX2g=",
+          "hasCaptcha": false,
+          "hideSave": true
+        },
+        {
+          "name": "江苏九州通医药有限公司",
+          "url": "http://flow.jztit.com/user/login.html",
+          "host": "flow.jztit.com",
+          "account": "hzzmhdcf",
+          "pwdCipher": "wq7DpsOhwqXCmcKrwr7DgsKmwpvCog==",
+          "hasCaptcha": false,
+          "hideSave": true
+        },
+        {
+          "name": "南京医药股份有限公司镇江分公司",
+          "url": "http://222.190.117.9/member/nyzj",
+          "host": "222.190.117.9",
+          "account": "hzzmhdzy",
+          "pwdCipher": "wq7DpsOhwqXCmMKnwqzCosKwwrzCrMKeXWRm",
+          "hasCaptcha": false,
+          "hideSave": true
+        }
+      ]
+    }
+  }
+};
