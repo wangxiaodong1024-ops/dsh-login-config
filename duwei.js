efa31706bffdc26e803f214cd76da72924714cf3
wangxiaodong1024-ops <wangxiaodong1024@gmail.com>
2026-08-24 18:23:48 +0800

    Update duwei.js

--- duwei.js
+++ duwei.js
@@ -5,9 +5,28 @@ window.__dsh3 = {
       "sites": [
         {
           "name": "南京医药(淮安)天颐有限公司",
+          "url": "http://222.190.117.9/member/haty",
           "host": "222.190.117.9",
           "account": "zhongmhd",
           "pwdCipher": "wq7DpsOhwqXCmMKnwqzCosKwwrzCrMKeXWRm",
+          "hasCaptcha": false,
+          "hideSave": true
+        },
+        {
+          "name": "江苏省医药有限公司",
+          "url": "https://lx.js-pc.com.cn/#/login",
+          "host": "lx.js-pc.com.cn",
+          "account": "hzzmhd",
+          "pwdCipher": "wq7DpsOhwqXCmMKnwqzCosKwwrw=",
+          "hasCaptcha": true,
+          "hideSave": true
+        },
+        {
+          "name": "宿迁瑞康医药有限公司",
+          "url": "http://222.187.213.150:8086/index/login/index.html",
+          "host": "222.187.213.150:8086",
+          "account": "中美华东",
+          "pwdCipher": "wq7DpsOhwqXCmMKnwqzCosKcwrPCv8Ks",
           "hasCaptcha": false,
           "hideSave": true
         }
