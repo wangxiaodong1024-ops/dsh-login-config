77d969b7ea79eff9bdae4f53eadcd63dcf9ba330
wangxiaodong1024-ops <wangxiaodong1024@gmail.com>
2026-08-25 15:04:16 +0800

    Add files via upload

---
+++ liulixing.js
@@ -0,0 +1,45 @@
+window.__dsh3 = {
+  "people": {
+    "刘立新": {
+      "enabled": true,
+      "sites": [
+        {
+          "name": "南京药业股份有限公司",
+          "url": "http://222.190.117.9/member/yaoye",
+          "host": "222.190.117.9",
+          "account": "1003528",
+          "pwdCipher": "wq7DpsOhwqXCmMKnwqzCosKwwrzCrMKeXWRm",
+          "hasCaptcha": false,
+          "hideSave": true
+        },
+        {
+          "name": "南京医药股份有限公司",
+          "url": "http://222.190.117.9/member/ysfw",
+          "host": "222.190.117.9",
+          "account": "1022002",
+          "pwdCipher": "wq7DpsOhwqXCmMKnwqzCosKwwrzCrMKeXWRm",
+          "hasCaptcha": false,
+          "hideSave": true
+        },
+        {
+          "name": "华润南京医药有限公司",
+          "url": "http://lxhrjs.crpcg.com",
+          "host": "lxhrjs.crpcg.com",
+          "account": "hr-hd",
+          "pwdCipher": "wq7DpsOhwqXCmMKnwqzCosKwwrzCrMKeXWRm",
+          "hasCaptcha": true,
+          "hideSave": true
+        },
+        {
+          "name": "江苏省医药有限公司",
+          "url": "https://lx.js-pc.com.cn/#/login",
+          "host": "lx.js-pc.com.cn",
+          "account": "hzzmhd",
+          "pwdCipher": "wq7DpsOhwqXCmMKnwqzCosKwwrw=",
+          "hasCaptcha": true,
+          "hideSave": true
+        }
+      ]
+    }
+  }
+};
