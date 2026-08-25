868879ae9e1eb67bd127ece2003e5284c2d4c790
wangxiaodong1024-ops <wangxiaodong1024@gmail.com>
2026-08-25 13:11:12 +0800

    Update caikehua.js

--- caikehua.js
+++ caikehua.js
@@ -2,7 +2,26 @@ window.__dsh3 = {
   "people": {
     "蔡可化": {
       "enabled": true,
-      "sites": []
+      "sites": [
+        {
+          "name": "国药控股徐州有限公司",
+          "url": "https://www1.igyxz.com/",
+          "host": "www1.igyxz.com",
+          "account": "hzzmhd",
+          "pwdCipher": "wq7DpsOhwqXCmcKowqrCucK5wqnCnsKcX2g=",
+          "hasCaptcha": true,
+          "hideSave": true
+        },
+        {
+          "name": "江苏华美医药有限责任公司",
+          "url": "http://www.jshmlx.com/vendor/login",
+          "host": "www.jshmlx.com",
+          "account": "zhangwei",
+          "pwdCipher": "wq7DpsOhwqXCmMKnwqzCosKwwrzCnsKcX2g=",
+          "hasCaptcha": false,
+          "hideSave": true
+        }
+      ]
     }
   }
 };
