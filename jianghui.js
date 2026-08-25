3f30d5115394a47706235b4a2d6e3f63dd1fe695
wangxiaodong1024-ops <wangxiaodong1024@gmail.com>
2026-08-25 17:32:09 +0800

    Update jianghui.js

--- jianghui.js
+++ jianghui.js
@@ -5,6 +5,7 @@ window.__dsh3 = {
       "sites": [
         {
           "name": "国药控股常州有限公司",
+          "url": "http://218.93.53.116:8086/sinopharmcz/client/userlogin.jsp",
           "host": "218.93.53.116:8086",
           "account": "ZMHDGS",
           "pwdCipher": "wq7DpsOhwqXCmcKowqrCucK5wqnCnsKcX2g=",
@@ -13,6 +14,7 @@ window.__dsh3 = {
         },
         {
           "name": "上药集团常州药业股份有限公司",
+          "url": "http://58.216.198.50:8012/",
           "host": "58.216.198.50:8012",
           "account": "凌岚",
           "pwdCipher": "wpvCpcKgYMKRwqs=",
@@ -21,6 +23,7 @@ window.__dsh3 = {
         },
         {
           "name": "南京药业股份有限公司",
+          "url": "http://222.190.117.9/member/yaoye",
           "host": "222.190.117.9",
           "account": "1003528",
           "pwdCipher": "wq7DpsOhwqXCmMKnwqzCosKwwrzCrMKeXWRm",
@@ -29,10 +32,20 @@ window.__dsh3 = {
         },
         {
           "name": "江苏省医药有限公司",
+          "url": "https://lx.js-pc.com.cn/#/login",
           "host": "lx.js-pc.com.cn",
           "account": "hzzmhd",
           "pwdCipher": "wq7DpsOhwqXCmMKnwqzCosKwwrw=",
           "hasCaptcha": true,
+          "hideSave": true
+        },
+        {
+          "name": "南京医药股份有限公司",
+          "url": "http://222.190.117.9/member/ysfw",
+          "host": "222.190.117.9",
+          "account": "1022002",
+          "pwdCipher": "wq7DpsOhwqXCmMKnwqzCosKwwrzCrMKeXWRm",
+          "hasCaptcha": false,
           "hideSave": true
         }
       ]
