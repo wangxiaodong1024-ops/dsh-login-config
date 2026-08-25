967ca74326c264ac7c555a1686c2552f14722fe4
wangxiaodong1024-ops <wangxiaodong1024@gmail.com>
2026-08-25 17:40:58 +0800

    Add files via upload

---
+++ zhouyue.js
@@ -0,0 +1,45 @@
+window.__dsh3 = {
+  "people": {
+    "周玥": {
+      "enabled": true,
+      "sites": [
+        {
+          "name": "国药控股常州有限公司",
+          "url": "http://218.93.53.116:8086/sinopharmcz/client/userlogin.jsp",
+          "host": "218.93.53.116:8086",
+          "account": "ZMHDGS",
+          "pwdCipher": "wq7DpsOhwqXCmcKowqrCucK5wqnCnsKcX2g=",
+          "hasCaptcha": true,
+          "hideSave": true
+        },
+        {
+          "name": "国药控股镇江有限公司",
+          "url": "http://flow.sinopharm-yz.com:8083/login.aspx",
+          "host": "flow.sinopharm-yz.com:8083",
+          "account": "杭州中美华东ZJ123",
+          "pwdCipher": "wq7DpsOhwqXCmcKowqrCucK5wqnCnsKcX2g=",
+          "hasCaptcha": false,
+          "hideSave": true
+        },
+        {
+          "name": "华润南通医药有限公司",
+          "url": "http://lxhrjs.crpcg.com",
+          "host": "lxhrjs.crpcg.com",
+          "account": "hzzm",
+          "pwdCipher": "wq7DpsOhwqXCmMKnwqzCosKwwrzCrMKeXWRm",
+          "hasCaptcha": true,
+          "hideSave": true
+        },
+        {
+          "name": "南京医药南通健桥有限公司",
+          "url": "http://222.190.117.9/member/ntjq",
+          "host": "222.190.117.9",
+          "account": "njjygf",
+          "pwdCipher": "wq7DpsOhwqXCmMKnwqzCosKwwrzCrMKeXWRm",
+          "hasCaptcha": false,
+          "hideSave": true
+        }
+      ]
+    }
+  }
+};
