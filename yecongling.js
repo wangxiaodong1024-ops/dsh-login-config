b2cc4ea9cebf53c6355e8b15841c8e5e6a6f5c75
wangxiaodong1024-ops <wangxiaodong1024@gmail.com>
2026-08-26 16:17:41 +0800

    Update yecongling.js

--- yecongling.js
+++ yecongling.js
@@ -22,15 +22,6 @@ window.__dsh3 = {
           "hideSave": true
         },
         {
-          "name": "南京医药股份有限公司镇江分公司",
-          "url": "http://222.190.117.9/member/nyzj",
-          "host": "222.190.117.9",
-          "account": "hzzmhdzy",
-          "pwdCipher": "wq7DpsOhwqXCmMKnwqzCosKwwrzCrMKeXWRm",
-          "hasCaptcha": false,
-          "hideSave": true
-        },
-        {
           "name": "南京医药股份有限公司",
           "url": "http://222.190.117.9/member/ysfw",
           "host": "222.190.117.9",
