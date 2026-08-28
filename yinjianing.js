979f852e0e948907d305a94023311b56ca4da061
wangxiaodong1024-ops <wangxiaodong1024@gmail.com>
2026-08-28 11:37:35 +0800

    Add files via upload

---
+++ yinjianing.js
@@ -0,0 +1,45 @@
+window.__dsh3 = {
+  "people": {
+    "殷家宁": {
+      "enabled": true,
+      "sites": [
+        {
+          "name": "江苏省医药有限公司",
+          "url": "https://lx.js-pc.com.cn/#/login",
+          "host": "lx.js-pc.com.cn",
+          "account": "hzzmhd",
+          "pwdCipher": "wq7DpsOhwqXCmMKnwqzCosKwwrw=",
+          "hasCaptcha": true,
+          "hideSave": true
+        },
+        {
+          "name": "南京药业股份有限公司",
+          "url": "http://222.190.117.9/member/yaoye",
+          "host": "222.190.117.9",
+          "account": "1003528",
+          "pwdCipher": "wq7DpsOhwqXCmMKnwqzCosKwwrzCrMKeXWRm",
+          "hasCaptcha": false,
+          "hideSave": true
+        },
+        {
+          "name": "国药控股扬州有限公司",
+          "url": "http://flow.sinopharm-yz.com:8081/login.aspx",
+          "host": "flow.sinopharm-yz.com:8081",
+          "account": "中美华东",
+          "pwdCipher": "wq7DpsOhwqXCmcKowqrCucK5wqnCnsKcX2g=",
+          "hasCaptcha": false,
+          "hideSave": true
+        },
+        {
+          "name": "国药控股泰州有限公司",
+          "url": "http://flow.sinopharm-yz.com:8082/login.aspx",
+          "host": "flow.sinopharm-yz.com:8082",
+          "account": "杭州中美华东",
+          "pwdCipher": "wqTCvcK7wobCucKtwqfCpcKmwrPCv8KeXWRm",
+          "hasCaptcha": false,
+          "hideSave": true
+        }
+      ]
+    }
+  }
+};
