d48ae4b5c4d33c9434375a1e15a8df391216333f
wangxiaodong1024-ops <wangxiaodong1024@gmail.com>
2026-08-31 16:27:34 +0800

    Update chenyazhou.js

--- chenyazhou.js
+++ chenyazhou.js
@@ -5,6 +5,7 @@ window.__dsh3 = {
       "sites": [
         {
           "name": "江苏省医药有限公司",
+          "url": "https://lx.js-pc.com.cn/#/login",
           "host": "lx.js-pc.com.cn",
           "account": "hzzmhd",
           "pwdCipher": "wq7DpsOhwqXCmMKnwqzCosKwwrw=",
@@ -13,6 +14,7 @@ window.__dsh3 = {
         },
         {
           "name": "南京药业股份有限公司",
+          "url": "http://222.190.117.9/member/yaoye",
           "host": "222.190.117.9",
           "account": "1003528",
           "pwdCipher": "wq7DpsOhwqXCmMKnwqzCosKwwrzCrMKeXWRm",
@@ -21,6 +23,7 @@ window.__dsh3 = {
         },
         {
           "name": "南京医药股份有限公司镇江分公司",
+          "url": "http://222.190.117.9/member/nyzj",
           "host": "222.190.117.9",
           "account": "hzzmhdzy",
           "pwdCipher": "wq7DpsOhwqXCmMKnwqzCosKwwrzCrMKeXWRm",
@@ -29,6 +32,7 @@ window.__dsh3 = {
         },
         {
           "name": "南京医药股份有限公司",
+          "url": "http://222.190.117.9/member/ysfw",
           "host": "222.190.117.9",
           "account": "1022002",
           "pwdCipher": "wq7DpsOhwqXCmMKnwqzCosKwwrzCrMKeXWRm",
@@ -37,6 +41,7 @@ window.__dsh3 = {
         },
         {
           "name": "国药控股镇江有限公司",
+          "url": "http://flow.sinopharm-yz.com:8083/login.aspx",
           "host": "flow.sinopharm-yz.com:8083",
           "account": "杭州中美华东ZJ123",
           "pwdCipher": "wq7DpsOhwqXCmcKowqrCucK5wqnCnsKcX2g=",
@@ -45,6 +50,7 @@ window.__dsh3 = {
         },
         {
           "name": "国药控股扬州有限公司",
+          "url": "http://flow.sinopharm-yz.com:8081/login.aspx",
           "host": "flow.sinopharm-yz.com:8081",
           "account": "中美华东",
           "pwdCipher": "wq7DpsOhwqXCmcKowqrCucK5wqnCnsKcX2g=",
@@ -53,6 +59,7 @@ window.__dsh3 = {
         },
         {
           "name": "华润扬州医药有限公司",
+          "url": "http://lxhrjs.crpcg.com",
           "host": "lxhrjs.crpcg.com",
           "account": "华东",
           "pwdCipher": "wq7DpsOhwqXCmMKnwqzCosKwwrzCrMKeXWRm",
@@ -61,6 +68,7 @@ window.__dsh3 = {
         },
         {
           "name": "江苏九州通医药有限公司",
+          "url": "http://flow.jztit.com/user/login.html",
           "host": "flow.jztit.com",
           "account": "hzzmhdcf",
           "pwdCipher": "wq7DpsOhwqXCmcKrwr7DgsKmwpvCog==",
@@ -69,6 +77,7 @@ window.__dsh3 = {
         },
         {
           "name": "华润江苏医药有限公司",
+          "url": "http://lxhrjs.crpcg.com",
           "host": "lxhrjs.crpcg.com",
           "account": "HZZMHD",
           "pwdCipher": "wq7DpsOhwqXCmMKnwqzCosKwwrzCrMKeXWRm",
@@ -77,10 +86,20 @@ window.__dsh3 = {
         },
         {
           "name": "上药控股镇江有限公司",
+          "url": "http://jsjtyy.com:20400",
           "host": "jsjtyy.com:20400",
           "account": "hzzmhd",
           "pwdCipher": "wpTCo8KYXcKRwqU=",
           "hasCaptcha": false,
+          "hideSave": true
+        },
+        {
+          "name": "华润镇江医药有限公司",
+          "url": "http://lxhrjs.crpcg.com",
+          "host": "lxhrjs.crpcg.com",
+          "account": "HZZM",
+          "pwdCipher": "wq7DpsOhwqXCmMKnwqzCosKwwrzCrMKeXWRm",
+          "hasCaptcha": true,
           "hideSave": true
         }
       ]
